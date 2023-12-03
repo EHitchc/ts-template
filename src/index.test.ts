5b37e34ea805fecbbfc4ee0623701aa362bdd4d1
Ethan Hitchcock <49368798+EHitchc@users.noreply.github.com>
2023-12-03 22:13:57 +1300

    fix: added mergeSort/quickSort for basic testing examples (#3)

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,8 +1,96 @@
-import { main } from '.';
+import { main, mergeSort, quickSort } from '.';
 
 test('main', () => {
 	const consoleSpy = jest.spyOn(console, 'log');
 	main();
 
 	expect(consoleSpy).toHaveBeenCalledWith('Hello World!');
+});
+
+describe('Merge sort', () => {
+	it('Merge sort with an empty array', () => {
+		const input: number[] = [];
+		const expectedOutput: number[] = [];
+		const result = mergeSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Merge sort with an array of one element', () => {
+		const input = [5];
+		const expectedOutput = [5];
+		const result = mergeSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Merge sort with an array in ascending order', () => {
+		const input = [1, 2, 3, 4, 5];
+		const expectedOutput = [1, 2, 3, 4, 5];
+		const result = mergeSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Merge sort with an array in descending order', () => {
+		const input = [5, 4, 3, 2, 1];
+		const expectedOutput = [1, 2, 3, 4, 5];
+		const result = mergeSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Merge sort with an array in random order', () => {
+		const input = [3, 1, 4, 2, 5];
+		const expectedOutput = [1, 2, 3, 4, 5];
+		const result = mergeSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Merge sort with an array in random order with some duplicates', () => {
+		const input = [3, 1, 4, 3, 2, 2, 5];
+		const expectedOutput = [1, 2, 2, 3, 3, 4, 5];
+		const result = mergeSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+});
+
+describe('Quick Sort', () => {
+	it('Quick sort with an empty array', () => {
+		const input: number[] = [];
+		const expectedOutput: number[] = [];
+		const result = quickSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Quick sort with an array of one element', () => {
+		const input = [5];
+		const expectedOutput = [5];
+		const result = quickSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Quick sort with an array in ascending order', () => {
+		const input = [1, 2, 3, 4, 5];
+		const expectedOutput = [1, 2, 3, 4, 5];
+		const result = quickSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Quick sort with an array in descending order', () => {
+		const input = [5, 4, 3, 2, 1];
+		const expectedOutput = [1, 2, 3, 4, 5];
+		const result = quickSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Quick sort with an array in random order', () => {
+		const input = [3, 1, 4, 2, 5];
+		const expectedOutput = [1, 2, 3, 4, 5];
+		const result = quickSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
+
+	it('Quick sort with an array in random order with some duplicates', () => {
+		const input = [3, 1, 4, 3, 2, 2, 5];
+		const expectedOutput = [1, 2, 2, 3, 3, 4, 5];
+		const result = quickSort(input);
+		expect(result).toEqual(expectedOutput);
+	});
 });
